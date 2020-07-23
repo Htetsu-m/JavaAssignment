9a781d737e7a34c81bbfb35f11c6b8f8275d51e3
Htetsu-m <65342474+Htetsu-m@users.noreply.github.com>
2020-07-23 11:07:28 +0630

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,20 +1,21 @@
 package Assignment1;
 
-public class Triangle {
-
-	public static void main(String[] args) {
+public class Triangle
+{
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
 		int x;
 		int y;
-		int z=0;
-		for(x=1;x<=5;x++) {
-			for(y=1;y<=x;y++) {
-				z=z+1;
-				System.out.print(z+" ");
+		int z = 0;
+		for(x = 1; x <= 5; x++)
+		{
+			for(y = 1; y <= x; y++)
+			{
+				z = z + 1;
+				System.out.print(z + " ");
 			}
 			System.out.println("");
 		}
-
 	}
-
 }
